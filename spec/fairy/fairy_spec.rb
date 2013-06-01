69a6135cac509fcd78b916191a0b15db9feee3ed
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2013-06-01 13:13:47 +0400

    empty test

---
+++ spec/fairy/fairy_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Fairy do
+  before do
+
+  end
+
+  it "hash" do
+    expect(1).to be 1
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'fairy'
+
+RSpec.configure do |config|
+  #config.include
+
+  config.order = "random"
+end
